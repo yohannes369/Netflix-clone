46462601d7604afc85ad6aeb17dd8b95b902a699
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-22 06:07:40 +0300

    header css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,32 @@
-import React from 'react'
-import "./header.css"
+import React from 'react';
+import "./header.css";
+
 function Header() {
   return (
     <div className="header_outer_container">
-    <div className="header_container"> 
+      <div className="header_container">
         <div className="header_left">
-            <ul>
-                  <li>Netflix</li>
-                    <li>HOME</li>
-                    <li>tV Shows</li>
-                    <li>mOVIES</li>
-                    <li>latest</li>  
-                    <li>mylist</li>  
-                    <li>browse laguages</li>  
-
-            </ul>
-            </div>   
-
-    </div>
-    <div className="header_right">
+          <ul>
+            <li>Netflix</li>
+            <li>HOME</li>
+            <li>TV Shows</li>
+            <li>MOVIES</li>
+            <li>Latest</li>
+            <li>My List</li>
+            <li>Browse Languages</li>
+          </ul>
+        </div>
+      </div>
+      <div className="header_right">
         <ul>
-            <li>searchIcon</li>
-            <li>notificationNoneIcon</li>
-            <li>ArrowBoxIcon</li>
-            <li>ArroDropDownIcon</li>
-            
+          <li>searchIcon</li>
+          <li>notificationNoneIcon</li>
+          <li>ArrowBoxIcon</li>
+          <li>ArrowDropDownIcon</li>
         </ul>
-
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
