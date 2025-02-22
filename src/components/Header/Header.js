af6bbcd46005ef75628e27444ecfa54ee045390f
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-22 06:50:13 +0300

    package icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import "./header.css";
 import netflix from "../../asset/image/netflix.png";
+import SearchIcon from '@mui/icons-material/Search';
+
 function Header() {
   return (
     <div className="header_outer_container">
       <div className="header_container">
         <div className="header_left">
           <ul>
-           <img src={netflix} alt="netflix" width="100" />
+            <img src={netflix} alt="netflix" width="100" />
             <li>HOME</li>
             <li>TV Shows</li>
             <li>MOVIES</li>
@@ -16,16 +18,15 @@ function Header() {
             <li>Browse Languages</li>
           </ul>
         </div>
-        {/* <div className="header_right">
-        <ul>
-          <li>searchIcon</li>
-          <li>notificationNoneIcon</li>
-          <li>ArrowBoxIcon</li>
-          <li>ArrowDropDownIcon</li>
-        </ul>
-      </div> */}
+        <div className="header_right">
+          <ul>
+            <li> <SearchIcon /></li>
+            {/* <li>notificationNoneIcon</li>
+            <li>ArrowBoxIcon</li>
+            <li>ArrowDropDownIcon</li> */}
+          </ul>
+        </div>
       </div>
-      
     </div>
   );
 }
