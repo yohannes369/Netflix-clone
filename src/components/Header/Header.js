e4e2ad915c3e15aec32705e022c57abd48176a50
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-22 06:26:22 +0300

    netflix logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import "./header.css";
-
+import netflix from "../../asset/image/netflix.png";
 function Header() {
   return (
     <div className="header_outer_container">
       <div className="header_container">
         <div className="header_left">
           <ul>
-            <li>Netflix</li>
+           <img src={netflix} alt="netflix" width="100" />
             <li>HOME</li>
             <li>TV Shows</li>
             <li>MOVIES</li>
@@ -16,15 +16,16 @@ function Header() {
             <li>Browse Languages</li>
           </ul>
         </div>
-      </div>
-      <div className="header_right">
+        {/* <div className="header_right">
         <ul>
           <li>searchIcon</li>
           <li>notificationNoneIcon</li>
           <li>ArrowBoxIcon</li>
           <li>ArrowDropDownIcon</li>
         </ul>
+      </div> */}
       </div>
+      
     </div>
   );
 }
