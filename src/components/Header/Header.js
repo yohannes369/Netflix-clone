ef95a6ca3d131d4fdcc756dede2089e54a91f0b7
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-22 05:39:31 +0300

    header css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,34 @@
 import React from 'react'
-
+import "./header.css"
 function Header() {
   return (
-    <div>Header</div>
+    <div className="header_outer_container">
+    <div className="header_container"> 
+        <div className="header_left">
+            <ul>
+                  <li>Netflix</li>
+                    <li>HOME</li>
+                    <li>tV Shows</li>
+                    <li>mOVIES</li>
+                    <li>latest</li>  
+                    <li>mylist</li>  
+                    <li>browse laguages</li>  
+
+            </ul>
+            </div>   
+
+    </div>
+    <div className="header_right">
+        <ul>
+            <li>searchIcon</li>
+            <li>notificationNoneIcon</li>
+            <li>ArrowBoxIcon</li>
+            <li>ArroDropDownIcon</li>
+            
+        </ul>
+
+    </div>
+    </div>
   )
 }
 
