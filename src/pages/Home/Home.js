843bb6c69540286d750c68440848b489d26a7b59
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-22 05:18:34 +0300

    header and footerr

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
 function Home() {
   return (
-    <div>Home</div>
+    <>
+        <Header />
+        <Footer />
+    </>
   )
 }
 
