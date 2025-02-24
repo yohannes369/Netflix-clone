aa2b04d4a3aff45e1e1c3a0196997fcce18794e3
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-24 10:55:31 +0300

    f

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,33 +2,38 @@ import React from 'react';
 import "./header.css";
 import netflix from "../../asset/image/netflix.png";
 import SearchIcon from '@mui/icons-material/Search';
-
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 function Header() {
-  return (
-    <div className="header_outer_container">
-      <div className="header_container">
-        <div className="header_left">
-          <ul>
-            <img src={netflix} alt="netflix" width="100" />
-            <li>HOME</li>
-            <li>TV Shows</li>
-            <li>MOVIES</li>
-            <li>Latest</li>
-            <li>My List</li>
-            <li>Browse Languages</li>
-          </ul>
+    return (
+        <div className="header_outer_container">
+            <div className="header_container">
+                {/* Left Side (Logo + Nav Links) */}
+                <div className="header_left">
+                    <ul>
+                        <li><img src={netflix} alt="netflix" width="100" /></li>
+                        <li>HOME</li>
+                        <li>TV Shows</li>
+                        <li>MOVIES</li>
+                        <li>Latest</li>
+                        <li>My List</li>
+                        <li>Browse Languages</li>
+                    </ul>
+                </div>
+
+                {/* Right Side (Search Icon) */}
+                <div className="header_right">
+                    <ul>
+                        <li><SearchIcon /></li>
+                        <li><NotificationsNoneIcon /></li>
+                        <li><AccountBoxIcon /></li>
+                        <li><ArrowDropDownIcon /></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div className="header_right">
-          <ul>
-            <li> <SearchIcon /></li>
-            {/* <li>notificationNoneIcon</li>
-            <li>ArrowBoxIcon</li>
-            <li>ArrowDropDownIcon</li> */}
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Header;
